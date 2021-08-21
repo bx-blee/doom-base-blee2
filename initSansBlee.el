80574582a63ec7f49b0a697ff6983d7b90c2f3ef
Mohsen Banan <form@mohsen.banan.1.byname.net>
2021-08-21 19:50:13 +0000

    init updates.

--- initSansBlee.el
+++ initSansBlee.el
@@ -154,7 +154,8 @@
        ;;purescript        ; javascript, but functional
        (python
         +lsp
-        +pyright)            ; beautiful is better than ugly
+        +pyright
+        +poetry)           ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
        ;;raku              ; the artist formerly known as perl6
